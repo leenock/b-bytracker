7f190bf3d2edd60c5a6f63e28a6e1329e8fff94c
leenock <leenock176@gmail.com>
2024-09-17 00:06:28 +0300

    backend commits done

--- backend/app.py
+++ backend/app.py
@@ -31,13 +31,46 @@ def home():
     return 'Hello, Flask!'
 
 # Define a route for creating an event
-@app.route('/event', methods=['POST'])
+@app.route('/events', methods=['POST'])
 def create_event():
     description = request.json['description']
     event = Event(description)
     db.session.add(event)
     db.session.commit()
     return format_event(event)
+
+# get all the events
+@app.route('/events', methods=['GET'])
+def get_events():
+    events= Event.query.order_by(Event.id.asc()).all()
+    event_list = []
+    for event in events:
+        event_list.append(format_event(event))
+    return {'events': event_list}
+
+# get single event
+@app.route('/events/<id>', methods=['GET'])
+def get_event(id):
+    event = Event.query.filter_by(id=id).one()
+    formatted_event = format_event(event)
+    return {'event': formatted_event}
+
+# Delete an event
+@app.route('/events/<id>', methods=['DELETE'])
+def delete_event(id):
+    event = Event.query.filter_by(id=id).one()
+    db.session.delete(event)
+    db.session.commit()
+    return f'Event (id: {id}) deleted'
+
+# update an Event
+@app.route('/events/<id>', methods=['PUT'])
+def update_event(id):
+    event = Event.query.filter_by(id=id)
+    description = request.json['description']
+    event.update(dict(description = description, created_at = datetime.utcnow()))
+    db.session.commit()
+    return {'event': format_event(event.one())}
 
 if __name__ == '__main__':
     app.run(debug=True)
